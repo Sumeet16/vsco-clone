42c2f7e14360d11665ba732224e3106b095e0888
Sumeet Yadav <sumeetishwar1610@gmail.com>
2022-12-05 21:26:09 +0530

    demo commit

--- src/components/Banner-1/Banner_1.jsx
+++ src/components/Banner-1/Banner_1.jsx
@@ -37,25 +37,27 @@ const Baneer_1 = () => {
             <p className="sub_heading">Trusted Community, Thoughtful Curation</p>
             <p className="light_heading">Connect to one of the largest creative communities in the world</p>
           </div>
-        </div>
-        <div className="home_wall">
-          <div class="home__wall-box" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <div class="home__wall-img-reveal-wrap" style={{ height: `${scrollPosition > 1430 ? scrollPosition > 1690 ? "100" : (260 - (1690 - scrollPosition)) / 260 * 100 : "0"}%` }}>
-              <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/62706115350c481cfa364290_ajschokora_after.png" loading="eager" class="home__wall-2-img" />
+          <div className="grp_div">
+            <div className="second">
+              <div class="demo_3" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="home__wall-img-reveal-wrap" style={{ height: `${scrollPosition > 1430 ? scrollPosition > 1690 ? "100" : (260 - (1690 - scrollPosition)) / 260 * 100 : "0"}%` }}>
+                  <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/62706115350c481cfa364290_ajschokora_after.png" loading="eager" class="home__wall-2-img" />
+                </div>
+                <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/62706114498ee36ab39cb9b4_ajschokora_before.png" loading="eager" class="home__wall-1-img" />
+              </div>
             </div>
-            <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/62706114498ee36ab39cb9b4_ajschokora_before.png" loading="eager" class="home__wall-1-img" />
-          </div>
-        </div>
 
-        <div className="home_wall_cont">
-          <div class="demo" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/627062161cf85b0411d11616_chillchlo.png" loading="eager" className="_2" />
-          </div>
-        </div>
+            <div className="first">
+              <div class="demo" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/627062161cf85b0411d11616_chillchlo.png" loading="eager" className="_2" />
+              </div>
+            </div>
 
-        <div className="home_wall_cont third">
-          <div class="demo_2" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/627c699cbd10c87d41590903_reedschick.png" loading="eager" className="_3" />
+            <div className="third">
+              <div class="demo_2" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <img src="https://assets-global.website-files.com/624de812dd74b622858823f2/627c699cbd10c87d41590903_reedschick.png" loading="eager" className="_3" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
