79842fb06108c5917f4d873f448670db0a7058f6
Sumeet Yadav <sumeetishwar1610@gmail.com>
2022-12-05 19:20:25 +0530

    banner-1 and video container added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import Baneer_1 from './components/Banner-1/Banner_1';
 import Footer from './components/Footer/Footer';
-import LandingPage from './components/LandingPage/LAndingPage';
+import LandingPage from './components/LandingPage/LandingPage';
 import VideoSlider from './components/VideoSlider/VideoSlider';
 
 function App() {
